280d3b0c9db8f3e937f795d3fb5859e47ea792ae
Amirol Zolkifli <sitehandy@gmail.com>
2024-11-27 16:00:15 +0800

    Latest update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class ProfileController extends Controller
 {
@@ -23,6 +24,30 @@ class ProfileController extends Controller
      */
     public function update(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email:filter'],
+            'phone' => ['required']
+        ]);
+
+        // Semak sekiranya input password diisi?
+        // Jika diisi, maka lakukan validasi password
+        // Attach password ke $data
+        if ($request->filled('password'))
+        {
+            $request->validate([
+                // 'password' => ['confirmed', Password::min(3)->uncompromised()]
+                'password' => ['confirmed', Password::min(3)]
+            ]);
+
+            $data['password'] = $request->input('password');
+        }
+
+        // Cari akaun User/Admin yang ingin dikemaskini
+        $admin = User::findOrFail(auth()->id());
+        // Update data
+        $admin->update($data);
+
+        return redirect()->route('profile.edit')->with('mesej-berjaya', 'Profile berjaya dikemaskini');
     }
 }
